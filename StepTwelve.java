728012d5a3ea89dd7a6242bd01c9899f0e80ada2
Lam Nguyen <lam.nguyen82@gmail.com>
2026-04-06 10:03:53 -0400

    fix: defined StepTwelvePointFive in its own file and removed it from StepTwelve

--- StepTwelve.java
+++ StepTwelve.java
@@ -46,27 +46,3 @@ public class StepTwelve {
  * internal logging and the math.
  */
 
-public class StepTwelvePointFive {
-
-  public static void main(String[] args) {
-
-    double orderOne = calculateConePrice(2, "Vanilla");
-    double orderTwo = calculateConePrice(3, "Chocolate");
-
-    System.out.println("Total for order one: $" + orderOne);
-    System.out.println("Total for order two: $" + orderTwo);
-  }
-
-  public static double calculateConePrice(int scoops, String flavor) {
-
-    double baseConePrice = 1.50;
-    double pricePerScoop = 2.25;
-
-    System.out.println("Prep: " + scoops + " Scoops of " + flavor);
-
-    double totalCost = baseConePrice + (scoops * pricePerScoop);
-
-    return totalCost;
-
-  } // end main
-} // end class
