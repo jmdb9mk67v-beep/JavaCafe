a5fa0ba6061391452115d1487858c5f70b176dae
Lam Nguyen <lam.nguyen82@gmail.com>
2026-04-07 10:38:06 -0400

    fix: sync local folder structure and remove ghost directory

---
+++ Dessert.java
@@ -0,0 +1,12 @@
+/**
+ * Picking a Random Dessert using SecureRandom
+ */
+
+import java.security.SecureRandom;
+
+public class Dessert {
+  private SecureRandom randNum = new SecureRandom();
+  private String[] names = {
+    "Fig Newton, Cheesecake, Pie, Donut, Gelato, Cookie, Cake"
+  }
+}
